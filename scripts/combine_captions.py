770005f1f3e575e5aa347f7bcc16795f4690d72b
liesen <liesen.dev@gmail.com>
2024-03-02 15:11:00 +0300

    refactor captions merger

--- scripts/combine_captions.py
+++ scripts/combine_captions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+
 from pathlib import Path
 
 
@@ -9,10 +10,10 @@ def combine_files(input_dir1, input_dir2, output_dir, meta_file=None, threshold=
     input_dir2 = Path(input_dir2)
     output_dir = Path(output_dir)
 
-    meta_tags = set()
+    meta_tags = []
     if meta_file is not None:
         with open(meta_file, 'r') as f:
-            meta_tags = set([x.strip() for x in f.readlines()])
+            meta_tags = [x.strip() for x in f.readlines()]
 
     if not output_dir.is_dir():
         output_dir.mkdir(parents=True)
@@ -20,20 +21,27 @@ def combine_files(input_dir1, input_dir2, output_dir, meta_file=None, threshold=
     for file1 in input_dir1.glob('*.txt'):
         file2 = input_dir2 / file1.name
 
-        if file2.exists():
-            output_file = output_dir / file1.name
-            combine_tags(file1, file2, output_file, meta_tags, threshold)
+        if not file2.exists():
+            continue
+
+        output_file = output_dir / file1.name
+
+        with open(file1, 'r') as f1, open(file2, 'r') as f2:
+            tags1 = [x.strip() for x in f1.read().split(',')]
+            tags2 = [x.strip() for x in f2.read().split(',')]
+            combine_tags(tags1, tags2, output_file, meta_tags, threshold)
 
 
-def combine_tags(file1, file2, output_file, meta_tags, threshold):
-    with open(file1, 'r') as f1, open(file2, 'r') as f2, open(output_file, 'w') as out_file:
-        tags1 = set([x.strip() for x in f1.read().split(',')])
-        tags2 = set([x.strip() for x in f2.read().split(',')])
-        if len(tags1 - set(meta_tags)) >= threshold and threshold != -1:
-            combined_tags = tags1 - set(meta_tags)
-        else:
-            combined_tags = tags1 | tags2 - set(meta_tags)
-        combined_tags = ', '.join(combined_tags)
+def combine_tags(tags1, tags2, output_file, meta_tags, threshold):
+
+    if len(tags1 - set(meta_tags)) >= threshold and threshold != -1:
+        combined_tags = set(tags1) - set(meta_tags)
+    else:
+        combined_tags = set(tags1) | set(tags2) - set(meta_tags)
+
+    combined_tags = ', '.join(combined_tags)
+
+    with open(output_file, 'w') as out_file:
         out_file.write(combined_tags)
 
 
