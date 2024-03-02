2450805c5318110d32dcef333a7fc74e9adeb3ab
liesen <liesen.dev@gmail.com>
2024-03-02 12:16:32 +0300

    add remove_text_orphans.py

---
+++ remove_text_orphans.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import argparse
+import pathlib
+
+img_suffixes = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".webp",
+    ".avif",
+    ".jxl"
+]
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Remove text files without corresponding images.")
+
+    parser.add_argument(
+        "directory", help="The directory to scan for text files and corresponding images.")
+
+    args = parser.parse_args()
+
+    directory = pathlib.Path(args.directory)
+
+    for file in directory.glob('*.txt'):
+        to_be_removed = True
+        for suffix in img_suffixes:
+            image_file = file.with_suffix(suffix)
+
+            if image_file.exists():
+                to_be_removed = False
+
+        if to_be_removed:
+            file.unlink()
+
+            print(
+                f"Deleted text file '{file.name}' because corresponding image file does not exist.")
+
+
+# Call the main function
+if __name__ == "__main__":
+    main()
