0031cf4fbaca2dd95de0a4a73a4978580361f112
liesen <liesen.dev@gmail.com>
2024-03-02 18:32:17 +0300

    revamp crawler

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -1,22 +1,61 @@
-from waifuc.action import FilterSimilarAction, ModeConvertAction, RandomFilenameAction
+import argparse
+
+from waifuc.action import FilterSimilarAction, \
+    ModeConvertAction, FirstNSelectAction, \
+    FileExtAction, AlignMaxAreaAction, \
+    MinAreaFilterAction
 from waifuc.export import TextualInversionExporter
 from waifuc.source import DanbooruSource
 
 
-def crawl(tags: list[str]):
+def crawl(tags: list[str], save_path, limit, key, username, align_images):
+    source = DanbooruSource(
+        tags, api_key=key, username=username, min_size=None)
 
-    s = DanbooruSource(tags)
+    images = source
 
-    # crawl images, process them, and then save them to directory with given format
-    s.attach(
+    if align_images:
+        images = images.attach(
+            MinAreaFilterAction(1024),
+            FileExtAction(ext='.png'),
+            AlignMaxAreaAction(1024),
+        )
+
+    images = images.attach(
         ModeConvertAction('RGB', 'white'),
         FilterSimilarAction('all'),  # filter duplicated images
-        RandomFilenameAction(ext='.png'),  # random rename files
-    ).export(
-        # save to surtr_dataset directory
-        TextualInversionExporter(tags[0].replace(':', ''), use_spaces=True)
+        FirstNSelectAction(limit),
+        # RandomFilenameAction(ext='.png'),  # random rename files
+    )
+
+    images.export(
+        # save to directory
+        TextualInversionExporter(save_path, use_spaces=True)
     )
 
 
 if __name__ == '__main__':
-    crawl(['sparkle_(honkai:_star_rail)'])
+    parser = argparse.ArgumentParser(
+        description='Grab pics from danbooru.')
+    parser.add_argument('-s', '--search', nargs='+', default=[],
+                        help="Search on danbooru. Use underscores. See https://danbooru.donmai.us/wiki_pages/help:cheatsheet")
+    parser.add_argument(
+        '-o', '--output-dir', help='Path to save the pictures to.', nargs='?', default="")
+    parser.add_argument(
+        '--limit', help='Select only this much images.', nargs='?', default=400, type=int)
+    parser.add_argument(
+        '--align-images', action='store_true',
+        help="Resize the images to have an area of 1024x1024 and filter them out if they aren't in highres.",)
+    parser.add_argument(
+        '--username', help='Danbooru username.', nargs='?', default=None)
+    parser.add_argument(
+        '--api-key', help='Danbooru API key.', nargs='?', default=None)
+    args = parser.parse_args()
+
+    if not args.output_dir:
+        args.output_dir = args.s[0]
+
+    args.output_dir = args.output_dir.replace(':', '')
+
+    crawl(args.search, args.output_dir, args.limit,
+          args.api_key, args.username, args.align_images)
