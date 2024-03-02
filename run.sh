763785994b5bd3417d1d03bba62c883a1ba1c0ae
liesen <liesen.dev@gmail.com>
2024-03-02 20:55:32 +0300

    change num_cpu_threads_per_process to 2

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ shift
 
 (
     trap 'kill 0' SIGINT;
-    accelerate launch --num_cpu_threads_per_process 8 sd-scripts/sdxl_train_network.py \
+    accelerate launch --num_cpu_threads_per_process 2 sd-scripts/sdxl_train_network.py \
     --config_file="$CONFIG/config.toml" \
     --dataset_config="$CONFIG/dataset.toml" \
     --log_with tensorboard --logging_dir="$LOGDIR" $@ \
