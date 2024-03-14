4f939ea0b5da4c46a98e8508697d85f889311541
liesen <liesen.dev@gmail.com>
2024-03-15 02:12:14 +0300

    also clean .npz files

--- scripts/remove_text_orphans.py
+++ scripts/remove_text_orphans.py
@@ -12,26 +12,32 @@ img_suffixes = [
     ".jxl"
 ]
 
+def rm(file: str):
+    to_be_removed = True
+
+    for suffix in img_suffixes:
+        image_file = file.with_suffix(suffix)
+
+        if image_file.exists():
+            to_be_removed = False
+
+    if to_be_removed:
+        file.unlink()
+
+        print(
+            f"Deleted file '{file.name}' because corresponding image file does not exist.")
+
 
 def main(args):
 
     directory = pathlib.Path(args.directory)
 
     for file in directory.glob('*.txt'):
-
-        to_be_removed = True
-
-        for suffix in img_suffixes:
-            image_file = file.with_suffix(suffix)
-
-            if image_file.exists():
-                to_be_removed = False
-
-        if to_be_removed:
-            file.unlink()
-
-            print(
-                f"Deleted text file '{file.name}' because corresponding image file does not exist.")
+        rm(file)
+    
+    if args.vae:
+        for file in directory.glob('*.npz'):
+            rm(file)
 
 
 if __name__ == "__main__":
@@ -41,6 +47,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "directory", help="The directory to scan for text files and corresponding images.")
+    parser.add_argument('--vae', action='store_true', help="Also remove the .npz files.")
 
     args = parser.parse_args()
 
