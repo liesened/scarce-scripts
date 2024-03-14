4372b5da900d5af85d1f50899a70d156a9df0043
liesen <liesen.dev@gmail.com>
2024-03-15 02:29:50 +0300

    refactor run.sh to support multiple config files in the same folder

--- run.sh
+++ run.sh
@@ -5,20 +5,30 @@ source sd-scripts/venv/bin/activate
 function usage {
     echo "$0 starts kohya trainer and tensorboard."
     echo "usage: $0 [config folder] <additional kohya options>"
+    exit
 }
 
-CONFIG=$1
-LOGDIR=$CONFIG/logs
+CONFIGDIR=$1
+LOGDIR=$CONFIGDIR/logs
 
 mkdir $LOGDIR
 
 shift
 
+function run_train {
+    for CONFIG in "$CONFIGDIR"/config*.toml; do
+        echo "Loading $CONFIG"
+        accelerate launch --num_cpu_threads_per_process 2 sd-scripts/sdxl_train_network.py \
+        --config_file="$CONFIG" \
+        --dataset_config="$CONFIGDIR/dataset.toml" \
+        --log_with tensorboard --logging_dir="$LOGDIR" $@
+    done
+}
+
+
+
 (
     trap 'kill 0' SIGINT;
-    accelerate launch --num_cpu_threads_per_process 2 sd-scripts/sdxl_train_network.py \
-    --config_file="$CONFIG/config.toml" \
-    --dataset_config="$CONFIG/dataset.toml" \
-    --log_with tensorboard --logging_dir="$LOGDIR" $@ \
-    & tensorboard --logdir="$LOGDIR" --host 0.0.0.0 & wait
+    run_train & tensorboard --logdir="$LOGDIR" --host 0.0.0.0 & wait
 )
+
